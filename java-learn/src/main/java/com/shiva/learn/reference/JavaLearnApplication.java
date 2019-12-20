cce38004c99bbb39b9b8d5fa6bafcb30ff3414f9
shivamahe <shivamahe@DESKTOP-D2TOKVQ.fios-router.home>
2019-12-20 00:52:55 -0500

    c

---
+++ java-learn/src/main/java/com/shiva/learn/reference/JavaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.learn.reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearnApplication.class, args);
+	}
+
+}
---
+++ java-learn/src/test/java/com/java/learn/javalearn/JavaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.learn.javalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
